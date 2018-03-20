1df60f72b7a4c4f79f77ce10d1ced9ed460dd99c
e <shenbaoone@gmail.com>
2018-03-20 22:58:21 +0800

    add gh-pages

--- src/app/pipe/interval-time/interval-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IntervalTimePipe } from './interval-time.pipe';
-
-describe('IntervalTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IntervalTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
